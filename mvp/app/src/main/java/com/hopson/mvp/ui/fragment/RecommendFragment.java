99df44f950d0eb291b43dd1f43fcc9a7c7cf6c07
heshenghui <557230@hopson.com.cn>
2020-06-25 16:23:51 +0800

    Retrofit + okhttp 库

--- mvp/app/src/main/java/com/hopson/mvp/ui/fragment/RecommendFragment.java
+++ mvp/app/src/main/java/com/hopson/mvp/ui/fragment/RecommendFragment.java
@@ -16,7 +16,7 @@ public class RecommendFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//       View  view = inflater.inflate(R.layout.f)
-        return super.onCreateView(inflater, container, savedInstanceState);
+       View  view = inflater.inflate(R.layout.fragment_recommend,container,false);
+        return view;
     }
 }
